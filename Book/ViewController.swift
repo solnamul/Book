80be086c58becd17e00540fdc2e43e156d7b3b1c
solnamul <sol7113@gmail.com>
2025-01-02 13:57:26 +0900

    Initial Commit

---
+++ Book/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Book
+//
+//  Created by t2023-m105 on 1/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BookTests/BookTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookTests.swift
+//  BookTests
+//
+//  Created by t2023-m105 on 1/2/25.
+//
+
+import Testing
+@testable import Book
+
+struct BookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
