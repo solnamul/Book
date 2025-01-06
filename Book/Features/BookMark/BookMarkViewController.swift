8752beab5b105634f688d40ef15681107cb09481
solnamul <sol7113@gmail.com>
2025-01-06 11:55:30 +0900

    feat: 북마크 기능 구현

--- Book/Features/BookMark/BookMarkViewController.swift
+++ Book/Features/BookMark/BookMarkViewController.swift
@@ -6,28 +6,57 @@
 //
 
 import UIKit
+import CoreData
 
-class BookMarkViewController: UIViewController {
+class BookMarkViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    private let label: UILabel = {
-        let label = UILabel()
-        label.text = "담은 책 리스트가 여기에 표시됩니다."
-        label.textAlignment = .center
-        label.font = .systemFont(ofSize: 18, weight: .medium)
-        return label
+    private var bookmarkedBooks: [NSManagedObject] = []
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "BookCell")
+        return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         setupUI()
+        fetchBookmarkedBooks()
     }
     
     private func setupUI() {
-        view.backgroundColor = .white
-        view.addSubview(label)
-        
-        label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    private func fetchBookmarkedBooks() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "BookEntity")
+        
+        do {
+            bookmarkedBooks = try context.fetch(fetchRequest)
+            tableView.reloadData()
+        } catch {
+            print("Core Data 불러오기 실패: \(error.localizedDescription)")
+        }
+    }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bookmarkedBooks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath)
+        let book = bookmarkedBooks[indexPath.row]
+        cell.textLabel?.text = book.value(forKey: "title") as? String
+        return cell
     }
 }
